9ac77eadd199b4864df6166bf72f865f1dea826d
joao deluchi <joao.deluchi@mercadolivre.com>
2023-08-13 19:10:14 -0300

    feat: startup

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "log"
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	//configProvider, err := runtime.NewEnv()
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//app, err := apirest.BuildApplication(
+	//	configProvider,
+	//	apirest.BuildRepositories(
+	//		configProvider))
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//return app.Run()
+}
